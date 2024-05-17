0518e57b5093e94308c912e75961ecf30fbf8477
Vicheak <suonvicheak991@gmail.com>
2024-05-17 23:37:06 +0700

    update project

---
+++ src/main/java/com/vicheak/coreapi/api/auth/web/AuthRestController.java
@@ -0,0 +1,50 @@
+package com.vicheak.coreapi.api.auth.web;
+
+import com.vicheak.coreapi.api.auth.AuthService;
+import com.vicheak.coreapi.base.BaseApi;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthRestController {
+
+    private final AuthService authService;
+
+    @PostAuthorize("permitAll()")
+    @PostMapping("/login")
+    public BaseApi<?> login(@Valid @RequestBody LoginDto loginDto){
+
+        AuthDto authDto = authService.login(loginDto);
+
+        return BaseApi.builder()
+                .isSuccess(true)
+                .code(HttpStatus.OK.value())
+                .message("You have logged in successfully!")
+                .timestamp(LocalDateTime.now())
+                .payload(authDto)
+                .build();
+    }
+
+    @PostAuthorize("permitAll()")
+    @PostMapping("/refresh")
+    public BaseApi<?> refresh(@Valid @RequestBody RefreshTokenDto refreshTokenDto){
+
+        AuthDto authDto = authService.refresh(refreshTokenDto);
+
+        return BaseApi.builder()
+                .isSuccess(true)
+                .code(HttpStatus.OK.value())
+                .message("Access Token has been renewed!")
+                .timestamp(LocalDateTime.now())
+                .payload(authDto)
+                .build();
+    }
+
+}
--- src/main/java/com/vicheak/coreapi/api/authority/web/AuthDto.java
+++
@@ -1,9 +0,0 @@
-package com.vicheak.coreapi.api.authority.web;
-
-import lombok.Builder;
-
-@Builder
-public record AuthDto(String tokenType,
-                      String accessToken,
-                      String refreshToken) {
-}
